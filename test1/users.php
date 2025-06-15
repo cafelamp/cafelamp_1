dd94f39e50aaff618ab61db5f699728dd3f0ef96
koshin1027 <143990605+koshin1027@users.noreply.github.com>
2025-06-15 19:01:55 +0900

    Delete test1 directory

--- test1/users.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    //仮のユーザー登録
-    $users = [
-        'user1' => password_hash('password1', PASSWORD_DEFAULT),
-        'admin' => password_hash('adminpass', PASSWORD_DEFAULT),
-    ];
-?>
